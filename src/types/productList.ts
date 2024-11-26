b9d36b17fd8c07102bcd7bbf8ac4e0533bc79cd1
박은정 <98pej11@gmail.com>
2024-11-26 15:20:13 +0900

    [chore] 불필요 파일 정리

--- src/types/productList.ts
+++
@@ -1,9 +0,0 @@
-// 상품 데이터 타입 정의
-export interface Product {
-  index: number; // 상품 아이디
-  amount: number; // 투자 금액
-  length: number; // 투자 기간
-  title: string; // 상품명
-  earningRate: number; // 수익률
-  thumbnail: string; // 썸네일 이미지 URL
-}
